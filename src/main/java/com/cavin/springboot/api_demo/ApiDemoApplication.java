fc7227c3f1b37457818a00084a7f1d188ce7354d
cavin <648404687@qq.com>
2018-03-29 13:46:12 +0800

    项目初始化

---
+++ src/main/java/com/cavin/springboot/api_demo/ApiDemoApplication.java
@@ -0,0 +1,12 @@
+package com.cavin.springboot.api_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDemoApplication.class, args);
+	}
+}
